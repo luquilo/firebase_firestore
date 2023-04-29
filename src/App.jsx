b6ad506f2a1587b4fb14656e8971e351c7d486d5
luquilo <luquilo.etc@gmail.com>
2023-04-29 09:42:16 +0700

    learn add doc nd delete doc in firebase 9

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,9 @@ import {
   collection, // used to make a collection of data from the firebase
   getDocs, // get the document from the firebase
   addDoc, // a method to adding a new data to the firebase
+  deleteDoc, // method to delete
+  doc, //similiar to collection
+
 } from "firebase/firestore";
 import BookForm from "./components/BookForm";
 import BookList from "./components/BookList";
@@ -23,10 +26,12 @@ function App() {
   };
 
   const [currentBooks, setCurrentBook] = useState([]);
-  const [childData, setChildData] = useState("kosong");
+  const [newBook, setNewBook] = useState("kosong");
+  const [deletedBookId, setDeletedBookId] = useState('')
 
   console.log(currentBooks);
-  console.log(childData);
+  console.log(newBook);
+  console.log(deletedBookId)
 
   // init firebase app
   initializeApp(firebaseConfig);
@@ -50,27 +55,38 @@ function App() {
       .catch((error) => console.log(error.message));
   }, []);
 
-  const addBookForm = async (data) => {
-    setChildData(data);
-    const book = await data;
+  const addBookForm = (data) => {
+    setNewBook(data);
+    const book = data;
     console.log(book);
     console.log(typeof book);
 
-    // console.log(childData)
-
-    setTimeout(() => {
-      addDoc(colRef, {
-        title: book.title,
-        author: book.author,
-      });// the second argument is the new document that we wanna to the firebase collection
-    }, 3000);
+    addDoc(colRef, {
+      title: book.title,
+      author: book.author,
+    }); // the second argument is the new document that we wanna to the firebase collection
   };
+
+  const deleteBookForm = (data) => {
+
+    setDeletedBookId(data)
+    const bookId = data
+    console.log(bookId)
+
+
+    // doc takes 3 argument, 1) the database 2) the collection name 3) id of the document
+    const docRef = doc(db, 'books', bookId) 
+    deleteDoc(docRef)
+    .then(()=>{
+      alert('selamat, anda berhasil menghapus 1 buku!')
+    })
+  }
 
   return (
     <div className="container">
       <BookForm addBookForm={addBookForm} />
       <h1>delete form</h1>
-      <BookDelete />
+      <BookDelete deleteBookForm={deleteBookForm}/>
       <BookList />
     </div>
   );
